e6d570b431b96e9ac0b1d7ab88eb6649602be244
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-06-26 00:06:51 +0300

    step 2

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function getRandomComputerResult() {
     const options = ["Rock", "Paper", "Scissors"];
-    return options[Math.floor(Math.random() * options.length)]
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex];
 }
-console.log(getRandomComputerResult());
+
+function hasPlayerWonTheRound(player, computer) {
+    if(player === "Rock" && computer==="Scissors") {
+        return true
+    }
+    if(player === "Scissors" && computer==="Paper") {
+        return true
+    }
+    if(player === "Paper" && computer==="Rock") {
+        return true
+    }
+    if (player === computer) {
+        return false
+    }
+}
+
+console.log(hasPlayerWonTheRound("Rock", "Scissors"));
+console.log(hasPlayerWonTheRound("Scissors", "Rock"));
